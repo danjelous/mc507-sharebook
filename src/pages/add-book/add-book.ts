eb95dec5fa14a69637d8f66692247404bfa68103
Danjelous <daniel.brunner@live.at>
2017-05-16 20:01:37 +0200

    Added addbook form

--- src/pages/add-book/add-book.ts
+++ src/pages/add-book/add-book.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 /**
  * Generated class for the AddBookPage page.
@@ -9,16 +10,38 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  */
 @IonicPage()
 @Component({
-  selector: 'page-add-book',
-  templateUrl: 'add-book.html',
+   selector: 'page-add-book',
+   templateUrl: 'add-book.html',
+
+   template: `
+    <form [formGroup]="searchForm" (ngSubmit)="searchByISBN()">
+      <ion-item>
+        <ion-label>Search by ISBN</ion-label>
+        <ion-input type="number" formControlName="isbn"></ion-input>
+      </ion-item>
+      <button ion-button type="submit" [disabled]="!searchForm.valid">Search</button>
+    </form>
+  `
 })
 export class AddBookPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+   // Default value of segments
+   addBookBy = 'search';
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AddBookPage');
-  }
+   // Searchform
+   private searchForm: FormGroup;
 
+   constructor(
+      public navCtrl: NavController,
+      public navParams: NavParams,
+      private formBuilder: FormBuilder) {
+
+      this.searchForm = this.formBuilder.group({
+         isbn: [ Validators.required]
+      });
+   }
+
+   searchByISBN() {
+      console.log("Here comes the service");
+   }
 }
