10deee9da7d6c2b85f0ec8f9c2d997d2cb5a2d43
Danjelous <daniel.brunner@live.at>
2017-06-01 14:34:38 +0200

    Add collection UI

--- src/pages/collection/collection.ts
+++ src/pages/collection/collection.ts
@@ -1,24 +1,33 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController } from 'ionic-angular';
+import { BookDetailsPage } from '../../pages/book-details/book-details';
+import { BooksOverviewProvider } from '../../providers/books-overview/books-overview';
+import { GenreOverviewPage } from "../../pages/genre-overview/genre-overview";
 
-/**
- * Generated class for the CollectionPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage()
 @Component({
-  selector: 'page-collection',
-  templateUrl: 'collection.html',
+   selector: 'page-collection',
+   templateUrl: 'collection.html',
 })
 export class CollectionPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+   booksOverview: any
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CollectionPage');
-  }
+   constructor(public navCtrl: NavController, private booksOverviewProvider: BooksOverviewProvider) {
+      this.booksOverviewProvider.getBooksOverview().then(data => {
 
+         let arr = [];
+         arr.push(data['1']);
+         arr.push(data['2']);
+         this.booksOverview = arr;
+      });
+   }
+
+   goToGenreOverview(genre) {
+      this.navCtrl.push(GenreOverviewPage, genre);
+   }
+
+   goToDetailsPage(book) {
+      this.navCtrl.push(BookDetailsPage, book);
+   }
 }
