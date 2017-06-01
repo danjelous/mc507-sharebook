150e8bf73b507ab24d36df436c8d5c05015c56e4
Danjelous <daniel.brunner@live.at>
2017-06-01 15:39:47 +0200

    Show menu button on home page after adding a book

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ViewController } from 'ionic-angular';
 import { BooksOverviewProvider } from '../../providers/books-overview/books-overview';
 import { GenreOverviewPage } from "../../pages/genre-overview/genre-overview";
 import { BookDetailsPage } from '../../pages/book-details/book-details';
@@ -13,14 +13,21 @@ export class HomePage {
    booksOverview: any;
    addedBook: string;
 
-   constructor(public navCtrl: NavController, public navParams: NavParams, private booksOverviewProvider: BooksOverviewProvider) {
+   constructor(public navCtrl: NavController, public navParams: NavParams,
+   private booksOverviewProvider: BooksOverviewProvider, private viewCtrl: ViewController) {
+
+      // Book added?
+      if (navParams.data.length > 1)
+         this.addedBook = navParams.data;
+
       this.booksOverviewProvider.getBooksOverview().then(data => {
          this.booksOverview = data;
-
-         if(navParams.data.length > 1) {
-            this.addedBook = navParams.data;
-         }
       });
+   }
+
+   ionViewWillEnter() {
+      this.viewCtrl.showBackButton(false);
+      this.navCtrl.setRoot(this.viewCtrl);
    }
 
    goToGenreOverview(genre) {
