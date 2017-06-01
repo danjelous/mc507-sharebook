8a0113e293581e38ddb1d2fdb3a13d85767b61f3
Danjelous <daniel.brunner@live.at>
2017-06-01 14:01:03 +0200

    Fancify BookData

--- src/pages/book-details/book-details.ts
+++ src/pages/book-details/book-details.ts
@@ -2,12 +2,6 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { BookDetailsProvider } from '../../providers/book-details/book-details';
 
-/**
- * Generated class for the BookDetailsPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage()
 @Component({
    selector: 'page-book-details',
@@ -20,16 +14,9 @@ export class BookDetailsPage {
 
    constructor(public navCtrl: NavController, public navParams: NavParams, private bookDetailsProvider: BookDetailsProvider) {
       this.bookId = navParams.data;
-      console.log(this.bookId);
 
       this.bookDetailsProvider.getBookDetails(this.bookId).then(data => {
-         console.log(data);
          this.bookData = data;
       });
    }
-
-   ionViewDidLoad() {
-      console.log('ionViewDidLoad BookDetailsPage');
-   }
-
 }
